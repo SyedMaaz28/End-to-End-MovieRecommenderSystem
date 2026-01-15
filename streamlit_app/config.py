cc89b690bc655bee2c6943b1234f8cec8da568d0
Syed Maaz <syedsam7676@gmail.com>
2026-01-15 16:32:00 +0530

    Configure Streamlit frontend for production

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,2 +1,2 @@
-API_BASE = "https://movie-rec-466x.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = "https://end-to-end-movierecommendersystem.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
